29c551e1a1b4a786876b39c967a0d9fbfb5ec26c
mauricio pantoja <mauriciop173@hotmail.com>
2023-06-21 20:08:34 -0500

    pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import Index from "../src/pages/Index";
-import Main from "./layouts/Main"
+import { RouterProvider } from "react-router-dom";
+import router from "./pages/router";
 function App() {
-
-  let data = [
-    {
-      title: 'Shonen',
-      character_photo: '../../src/assets/icons/AvatarCarousel.svg',
-      cover_photo: '../../src/assets/icons/ImgCarrusel.png',
-      description: 'Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.'
-    }
-  ]
   return (
-    <div className='flex flex-col min-h-screen items-center justify-between bg-[url("../../src/assets/images/Background.png")] bg-center bg-cover
-		md:bg-none md:bg-white'>
-        <Index data={data} />
-    </div>
-
+   <RouterProvider router={router}/>
   )
 }
 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,19 +1,19 @@
 import Carousel from "../componets/Carousel";
 import Welcome from "../componets/Welcome";
 import Main from "../layouts/Main";
-export default function Index({ data }) {
+import FormInput from "../componets/FormInput"
+import NavBar from "../componets/NavBar";
+export default function Index() {
     return (
         <Main>
             <main className='flex flex-col flex-grow min-h-screen md:bg-[url("../../src/assets/images/Background1.png")] w-full justify-evenly items-center pt-20 px-5  '>
-                <Welcome />
-                
+                {/* <Welcome /> */}
+
+                <FormInput />
             </main>
-            <Carousel
-                    title={data[0].title}
-                    character_photo={data[0].character_photo}
-                    cover_photo={data[0].cover_photo}
-                    pepe={data[0].description}
-                />
+            {/* <Carousel}
+                /> */}
+                
         </Main >
     )
 }
